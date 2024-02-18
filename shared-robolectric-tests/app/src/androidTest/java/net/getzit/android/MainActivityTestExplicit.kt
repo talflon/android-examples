674968df693da072b4602145bca634803409484e
Daniel Getz <dan@getzit.net>
2024-02-18 12:23:02 -0500

    simplify test rules by using a single createAndroidComposeRule<MainActivity> - fixes app:testReleaseUnitTest for shared test version. Explicit version still   fails with "Unable to resolve activity for Intent"

--- shared-robolectric-tests/app/src/androidTest/java/net/getzit/android/MainActivityTestExplicit.kt
+++ shared-robolectric-tests/app/src/androidTest/java/net/getzit/android/MainActivityTestExplicit.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasClickAction
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.performClick
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -16,14 +14,11 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTestExplicit {
-    @get:Rule(order = 0)
-    val composeRule = createComposeRule()
-
-    @get:Rule(order = 1)
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule
+    val composeRule = createAndroidComposeRule<MainActivity>()
 
     private val context: Context
-        get() = ApplicationProvider.getApplicationContext<MainActivity>()
+        get() = composeRule.activity.applicationContext
 
     private fun getString(@StringRes id: Int) = context.getString(id)
 
--- shared-robolectric-tests/shared-test/src/main/java/net/getzit/android/MainActivityTester.kt
+++ shared-robolectric-tests/shared-test/src/main/java/net/getzit/android/MainActivityTester.kt
@@ -4,23 +4,18 @@ import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasClickAction
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.performClick
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
 abstract class MainActivityTester {
-    @get:Rule(order = 0)
-    val composeRule = createComposeRule()
+    @get:Rule
+    val composeRule = createAndroidComposeRule<MainActivity>()
 
-    @get:Rule(order = 1)
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-
-    protected val context: Context
-        get() = ApplicationProvider.getApplicationContext<MainActivity>()
+    private val context: Context
+        get() = composeRule.activity.applicationContext
 
     protected fun getString(@StringRes id: Int) = context.getString(id)
 
