51c52b983e3bcf08e448b9dfac21c80eecfeb2f1
Daniel Getz <dan@getzit.net>
2024-08-25 20:00:44 -0400

    add compose compiler plugin and update Kotlin library to 2.0.20

--- shared-robolectric-tests/app/build.gradle.kts
+++ shared-robolectric-tests/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -37,9 +38,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
     }
 
     testOptions {
